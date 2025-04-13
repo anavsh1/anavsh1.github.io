1c04e65a68c961a7c984c529e7fc2c4c8a61961a
anavsh1 <anavsh0829@gmail.com>
2025-04-13 01:46:43 -0400

    Website revamp and content update

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ var i;
 for (i = 0; i < coll.length; i++) {
   coll[i].addEventListener("click", function() {
     this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.maxHeight){
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
+    // Get all exp-desc elements for this company
+    var nextSibling = this.nextElementSibling;
+    while (nextSibling && nextSibling.classList.contains("exp-desc")) {
+      nextSibling.classList.toggle("active");
+      nextSibling = nextSibling.nextElementSibling;
     }
   });
 }
